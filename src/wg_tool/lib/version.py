df62c101e5802fa470c236d9133ae92fa8cf5cad
Gene C <arch@sapience.com>
2024-01-19 11:52:15 -0500

    version now includes released vs development

--- src/wg_tool/lib/version.py
+++ src/wg_tool/lib/version.py
@@ -5,10 +5,11 @@ Project wg_tool
 """
 from .msg import info_msg
 
-__version__ = "6.5.0"
+__version__ = "6.6.0"
 __date__ = "2024-01-19"
+__reldev__ = "released"
 
 def version() -> None:
     """ report version and release date """
-    vers = f'wg-tool: version {__version__} ({__date__})'
+    vers = f'wg-tool: version {__version__} ({__reldev__}, {__date__})'
     info_msg(f'{vers}')
